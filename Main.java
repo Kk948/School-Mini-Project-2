5a44701f9436d4ca797e78c9d93db5b767aa14bd
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-10-08 16:55:31 +0000

    I added fullmoon=true to the second if statement

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ class Coven {
   {
     boolean fullMoon = true;
     int witch = 7;
-    if (witch= 13 || fullMoon = true)
+    if (witch = 13 || fullMoon = true)
+    {
+      System.out.println("Let's the encantation begin");
+      if(fullMoon=true)
+      {
+         system.out.println("some witches are missing from the coven");
+         System.out.println("Let's brew some potions instead");
+      }
+    }else
+      {System.out.println("The meeting is canceled today");
+
+    }
   }
 }
