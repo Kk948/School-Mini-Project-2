795bf396e310be2ee4df5d8e4a55df40d534d918
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-10-08 17:17:56 +0000

    I added a for loops

--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
-public class Coven {
+public class Main
+{
   public static void main(String[] args) 
   {
     boolean fullMoon = true;
     int witch = 7;
-    if (witch <= 13 && fullMoon)
+    //There are 13 witches in the coven
+    if (witch == 13 && fullMoon)
     {
       System.out.println("Let's the encantation begin");
-      if(fullMoon=true)
-      {
-         System.out.println("some witches are missing from the coven");
-         System.out.println("Let's brew some potions instead");
-      }
-    }else
-      {System.out.println("The meeting is canceled today");
-
     }
+    else  if(fullMoon)
+    {
+        System.out.println("Some witches are missing from the coven");
+        System.out.println("Let's brew some potions instead");
+        for (p=0;p < 24;p++) {
+          System.out.println("The Coven made"+ p + potions);
+  }
+    }
+    else
+      {
+        System.out.println("The meeting is canceled today");
+
+      }
   }
 }
