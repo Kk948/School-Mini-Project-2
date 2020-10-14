1c023329b839086771493204b32718ce13ee42b6
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2020-10-14 13:38:10 +0000

    I added more comments

--- Main.java
+++ Main.java
@@ -7,18 +7,22 @@ public class Main
     boolean fullMoon = true;
     int witch = 7;
     //There are 13 witches in the coven
+    //The code states if the moon is full and there are 13 witches then the encantation will begin
     if (witch == 13 && fullMoon)
     {
       System.out.println("Let's the encantation begin");
     }
     else  if(fullMoon)
+    // if there is only a full moon they'll brew some potions instead
     {
         System.out.println("Some witches are missing from the coven");
         System.out.println("Let's brew some potions instead");
+        //The code prints out potions til the number 6
         for (int potions=0;potions < 6 ;potions++) {
           System.out.println("The Coven made "+ potions + " potions");
   }
     }
+    //lastly if it doesn't meet the requirements then the metting is cancelled
     else
       {
         System.out.println("The meeting is canceled today");
@@ -32,7 +36,7 @@ public class Main
       System.out.println("What's your age?");
       int age = input.nextInt();
 
-      System.out.println("what's your name young witch");
+      System.out.println("What's your name young witch ?");
       String name=Scan.next();
 
 
@@ -40,7 +44,7 @@ public class Main
       String covenResp = Scan.next();
       
       System.out.println("Welcome to the Coven! "+ name);
-      System.out.println(" We hope you'll  have a great time with our other " + age + " year old witches!");
+      System.out.println(" We hope you'll have a great time with our other " + age + " year old witches!");
 
   }
   
